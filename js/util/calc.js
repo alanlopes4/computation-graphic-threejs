8439e6960a5919b0a16a9c47099e45d1266fc1ce
Alan Lopes <alan.lopes@eurekalabs.com.br>
2019-07-15 20:58:59 -0300

    feat: WIP calc.js

--- js/util/calc.js
+++ js/util/calc.js
@@ -1,8 +1,3 @@
-var calc_ponto_vista = (a, b, c) => ({
-  a,
-  b,
-  c
-});
 var mobj_cubo = {
   v1: [],
   v2: [],
@@ -14,23 +9,8 @@ var mobj_cubo = {
   v8: []
 };
 
-var plano = {
-  p1: { i: 0, j: 0, k: 0 },
-  p2: { i: 0, j: 0, k: 0 },
-  p3: { i: 0, j: 0, k: 0 }
-};
-var ponto = { x0: 0, y0: 0, z0: 0 };
-
+var ponto = { x: 0, y: 0, z: 0 };
 var dispositivo = { resH: 0, resV: 0 };
-
-//CALCULO D0, D1, D
-var d0 = () =>
-  normal.nx * ponto.x0 + normal.ny * ponto.y0 + normal.nz * ponto.z0;
-var d1 = () =>
-  normal.nx * ponto_vista.a +
-  normal.ny * ponto_vista.b +
-  normal.nz * ponto_vista.c;
-var d = () => d0 - d1;
 
 var Xmax = 0;
 var Xmin = 0;
@@ -61,21 +41,19 @@ var matriz_janela_viewport = (Sx, Sy, Xmin, Umin, Ymax, Vmin) => {
   return matriz;
 };
 
-var coords_dispositivo_truncadas = { x: [], y: [], w: [] };
-
 //###################################################
 
-var ponto_vista_wcs = { a: 0, b: 0, c: 0 };
+var get_ponto_vista = () => ({ a: 0, b: 0, c: 0 });
 
-var plano_projecao = {
-  r0: { x0: 0, y0: 0, z0: 0 },
-  p1: { x1: 0, y1: 0, z1: 0 },
-  p2: { x2: 0, y2: 0, z2: 0 },
-  p3: { x3: 0, y3: 0, z3: 0 }
-};
+var get_plano_projecao = (r0, p1, p2, p3) => ({
+  r0, //pontoPlano
+  p1, //ponto1
+  p2, //ponto2
+  p3 //ponto3
+});
 
 var coords_vertice = { x: 0, y: 0, z: 0 };
-var calc_dados_objeto = coordenadas_vertice => ({
+var dados_objeto = coordenadas_vertice => ({
   coordenadas_vertice: coordenadas_vertice,
   numero_vertices: coordenadas_vertice.length,
   numero_superficies: 0,
@@ -83,51 +61,100 @@ var calc_dados_objeto = coordenadas_vertice => ({
   vertice_determinada_superficie: []
 });
 //Cálculo do vetor normal
-var vet_normal = plano_projecao => {
+var calc_vet_normal = plano_projecao => {
   let n = {};
-  n.nx =
-    (plano_projecao.p1.y1 - plano_projecao.p2.y2) *
-      (plano_projecao.p3.z3 - plano_projecao.p2.z2) -
-    (plano_projecao.p3.y3 - plano_projecao.p2.y2) *
-      (plano_projecao.p1.z1 - plano_projecao.p2.z2);
-  n.ny =
-    -(plano_projecao.p1.x1 - plano_projecao.p2.x2) *
-      (plano_projecao.p3.z3 - plano_projecao.p2.z2) -
-    (plano_projecao.p3.x3 - plano_projecao.p2.x2) *
-      (plano_projecao.p1.z1 - plano_projecao.p2.z2);
-  n.nz =
-    (plano_projecao.p1.x1 - plano_projecao.p2.x2) *
-      (plano_projecao.p3.y3 - plano_projecao.p2.y2) -
-    (plano_projecao.p3.x3 - plano_projecao.p2.x2) *
-      (plano_projecao.p1.y1 - plano_projecao.p2.y2);
+  n.nx = //normal[0]
+    (plano_projecao.p1.y - plano_projecao.p2.y) *
+      (plano_projecao.p3.z - plano_projecao.p2.z) -
+    (plano_projecao.p3.y - plano_projecao.p2.y) *
+      (plano_projecao.p1.z - plano_projecao.p2.z);
+  n.ny = //normal[1]
+    -(plano_projecao.p1.x - plano_projecao.p2.x) *
+      (plano_projecao.p3.z - plano_projecao.p2.z) -
+    (plano_projecao.p3.x - plano_projecao.p2.x) *
+      (plano_projecao.p1.z - plano_projecao.p2.z);
+  n.nz = //normal[2]
+    (plano_projecao.p1.x - plano_projecao.p2.x) *
+      (plano_projecao.p3.y - plano_projecao.p2.y) -
+    (plano_projecao.p3.x - plano_projecao.p2.x) *
+      (plano_projecao.p1.y - plano_projecao.p2.y);
 
-  return n;
+  return {
+    nx: n.nx == -0 ? 0 : n.nx,
+    ny: n.ny == -0 ? 0 : n.ny,
+    nz: n.nz == -0 ? 0 : n.nz
+  };
 };
+
+//CALCULO D0, D1, D
+var calc_d0 = (normal, plano_projecao) =>
+  normal.nx * plano_projecao.r0.x +
+  normal.ny * plano_projecao.r0.y +
+  normal.nz * plano_projecao.r0.z;
+var calc_d1 = (normal, ponto_vista) =>
+  normal.nx * ponto_vista.a +
+  normal.ny * ponto_vista.b +
+  normal.nz * ponto_vista.c;
+var calc_d = (d0, d1) => d0 - d1;
+
 //Calculo da Matriz perspectiva
-var matriz_perspectiva = () => {
+var calc_matriz_perspectiva = (d, d0, d1, normal, ponto_vista) => {
   let matriz = [];
   //LINHA 1
-  matriz[0][0] = d() + ponto_vista.a * vet_normal().nx;
-  matriz[0][1] = ponto_vista.a * vet_normal().ny;
-  matriz[0][2] = ponto_vista.a * vet_normal().nz;
-  matriz[0][3] = -ponto_vista.a * d0();
+  matriz[0] = [
+    d + ponto_vista.a * normal.nx,
+    ponto_vista.a * normal.ny,
+    ponto_vista.a * normal.nz,
+    -ponto_vista.a * d0
+  ];
   //LINHA 2
-  matriz[1][0] = ponto_vista.b * vet_normal().nx;
-  matriz[1][1] = d() + ponto_vista.b * vet_normal().ny;
-  matriz[1][2] = ponto_vista.b * vet_normal().nz;
-  matriz[1][3] = -ponto_vista.b * d0();
+  matriz[1] = [
+    ponto_vista.b * normal.nx,
+    d + ponto_vista.b * normal.ny,
+    ponto_vista.b * normal.nz,
+    -ponto_vista.b * d0
+  ];
   //LINHA 3
-  matriz[2][0] = ponto_vista.c * vet_normal().nx;
-  matriz[2][1] = ponto_vista.c * vet_normal().ny;
-  matriz[2][2] = d + ponto_vista.c * vet_normal().nz;
-  matriz[2][3] = -ponto_vista.c * d0();
+  matriz[2] = [
+    ponto_vista.c * normal.nx,
+    ponto_vista.c * normal.ny,
+    d + ponto_vista.c * normal.nz,
+    -ponto_vista.c * d0
+  ];
   //LINHA 4
-  matriz[3][0] = vet_normal().nx;
-  matriz[3][1] = vet_normal().ny;
-  matriz[3][2] = vet_normal().nz;
-  matriz[3][3] = -d1();
+  matriz[3] = [normal.nx, normal.ny, normal.nz, -d1];
 
-  return matriz;
+  return matriz.map(linha =>
+    linha.map(v => {
+      if (v == -0) return 0;
+      return v;
+    })
+  );
+};
+
+//Cálculo da Matriz de projeção
+var calc_matriz_projecao = dados_objeto => {
+  let matriz_projecao = [];
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < dados_objeto.numero_vertices; j++) {
+      matriz_projecao[i][j] = 0;
+      for (let k = 0; k < 4; k++)
+        matriz_projecao[i][j] +=
+          matriz_projecao[i][k] * dados_objeto.coordenadas_vertice[k][j];
+    }
+  }
+  return matriz_projecao;
+};
+//Realiza o cálculo da transformação homogenea
+var calc_transf_homogenea = (dados_objeto, matriz_projecao) => {
+  let matriz_homogenea = [];
+  for (let i = 0; i < dados_objeto.numero_vertices; i++) {
+    let valorW = matriz_projecao[3][i];
+    for (let j = 0; j < 4; j++) {
+      matriz_homogenea[j][i] = matriz_projecao[j][i] / valorW;
+      if (matriz_homogenea[j][i] == -0) matriz_homogenea[j][i] = 0;
+    }
+  }
 };
 
 //Calculo das Coordenadas no Plano de Projeção
@@ -143,24 +170,42 @@ function transformacao_coords_cartesianas(coords_homogeneas) {
 function test() {
   console.log("TESTE COMECANDO");
 
-  let ponto_vista = calc_ponto_vista(8, 2, 10);
-  let m_objeto_cubo = 
+  let ponto_vista = get_ponto_vista(0, 0, 5);
+  let plano_projecao = get_plano_projecao(
+    { x: 0, y: 0, z: 0 }, //r0
+    { x: 1, y: 0, z: 0 }, //p1
+    { x: 0, y: 0, z: 0 }, //p2
+    { x: 0, y: 1, z: 0 } //p3
+  );
+
+  let normal = calc_vet_normal(plano_projecao);
+  let d0 = calc_d0(normal, plano_projecao);
+  let d1 = calc_d1(normal, ponto_vista);
+  let d = calc_d(d0, d1);
+
+  let matriz_perspectiva = calc_matriz_perspectiva(
+    d,
+    d0,
+    d1,
+    normal,
+    ponto_vista
+  );
+  console.log(matriz_perspectiva);
 }
 
 test();
 
-
-function coords_vertice_cubo () {
+function coords_vertice_cubo() {
   let cubo = [];
 
-  cubo[0] = {x: 0, y: 0, z:0, w: 1};
-  cubo[1] = {x: 1, y: 0, z:0, w: 1};
-  cubo[2] = {x: 1, y: 0, z:1, w: 1};
-  cubo[3] = {x: 0, y: 0, z:1, w: 1};
-  cubo[4] = {x: 0, y: 1, z:1, w: 1};
-  cubo[5] = {x: 1, y: 1, z:1, w: 1};
-  cubo[6] = {x: 1, y: 1, z:0, w: 1};
-  cubo[7] = {x: 0, y: 1, z:0, w: 1};
+  cubo[0] = { x: 0, y: 0, z: 0, w: 1 };
+  cubo[1] = { x: 1, y: 0, z: 0, w: 1 };
+  cubo[2] = { x: 1, y: 0, z: 1, w: 1 };
+  cubo[3] = { x: 0, y: 0, z: 1, w: 1 };
+  cubo[4] = { x: 0, y: 1, z: 1, w: 1 };
+  cubo[5] = { x: 1, y: 1, z: 1, w: 1 };
+  cubo[6] = { x: 1, y: 1, z: 0, w: 1 };
+  cubo[7] = { x: 0, y: 1, z: 0, w: 1 };
 
   return cubo;
 }
@@ -168,12 +213,12 @@ function coords_vertice_cubo () {
 function coords_vertice_superfice_cubo() {
   let superficie = [];
 
-  superficie[0] = {a: 4, b: 3, c:6, d: 5, e: 4};
-  superficie[1] = {a: 3, b: 2, c:7, d: 6, e: 3};
-  superficie[2] = {a: 8, b: 7, c:2, d: 1, e: 8};
-  superficie[3] = {a: 5, b: 8, c:1, d: 4, e: 5};
-  superficie[4] = {a: 5, b: 6, c:7, d: 8, e: 5};
-  superficie[5] = {a: 1, b: 2, c:3, d: 4, e: 1};
+  superficie[0] = { a: 4, b: 3, c: 6, d: 5, e: 4 };
+  superficie[1] = { a: 3, b: 2, c: 7, d: 6, e: 3 };
+  superficie[2] = { a: 8, b: 7, c: 2, d: 1, e: 8 };
+  superficie[3] = { a: 5, b: 8, c: 1, d: 4, e: 5 };
+  superficie[4] = { a: 5, b: 6, c: 7, d: 8, e: 5 };
+  superficie[5] = { a: 1, b: 2, c: 3, d: 4, e: 1 };
 
   return superficie;
 }
